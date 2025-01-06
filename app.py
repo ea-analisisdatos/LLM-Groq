952efc61db3c986d584c1d41adc8cd7a953751fc
Erika Alvares <129653696+ea-analisisdatos@users.noreply.github.com>
2025-01-06 13:09:45 +0000

    requirements.txt y actualización de los modelos Gamma ha sido desactualizada en la web de Groq

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ groq_api_key = os.getenv("groq_api_key")
 st.sidebar.title("Personalization")
 prompt = st.sidebar.title("System Prompt: ")
 model = st.sidebar.selectbox(
-    'Choose a model', ['Llama3-8b-8129', 'Llama3-70b-8192', 'Mixtral-8x7b-32768', 'Gemma-7b-It']
+    'Choose a model', ['Llama3-8b-8192', 'Llama3-70b-8192', 'Mixtral-8x7b-32768']
 )
 
 # Groq client
@@ -18,17 +18,17 @@ client = Groq(api_key = groq_api_key)
 # Streamlit Interface
 st.title("💬 Chat with Groq's LLM")
 
-# Initialize session state for history
+# Initialize sessesion state for history
 if "history" not in st.session_state:
     st.session_state.history = []
 
 user_input = st.text_input("Enter your query: ", "")
 
-if st.button("Submit"): 
+if st.button("Submit"):
     chat_completion = client.chat.completions.create(
         messages=[
             {
-                "role" : "user"
+                "role" : "user",
                 "content" : user_input,
             }
         ],
@@ -39,11 +39,11 @@ if st.button("Submit"):
     st.session_state.history.append({"query" : user_input, "response" : response})
 
     # Display the response
-    st.markdown(f'<div class="response-box">{response}</div', unsafe_allow_html=True)
+    st.markdown(f'<div class="response-box">{response}</div>', unsafe_allow_html=True)
 
 # Display history
-st.sidebard.title("History")
+st.sidebar.title("History")
 for i, entry in enumerate(st.session_state.history):
-    if st.siderbar.button(f'Query {i+1}: {entry["query"]}'):
-        st.markdown(f'<div class="response-box">{entry["response"]}</div', unsafe_allow_html=True)
-
+    if st.sidebar.button(f'Query {i+1}: {entry["query"]}'):
+        st.markdown(f'<div class="response-box">{entry["response"]}</div>', unsafe_allow_html=True)
+        
